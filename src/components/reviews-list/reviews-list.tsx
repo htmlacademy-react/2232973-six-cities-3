9aaf0ab1bc1f38ac644eeb7f9a5a7ba85edef93c
keka <kvtka4enko@yandex.ru>
2025-07-02 21:45:53 +0300

    feat(reviews-list): ограничит количество отображаемых ревью

--- src/components/reviews-list/reviews-list.tsx
+++ src/components/reviews-list/reviews-list.tsx
@@ -3,6 +3,8 @@ import ReviewItem from '../review/review-item';
 import { Review } from '../../types/reviews';
 import { AuthorizationStatus } from '@/const';
 
+const MAX_REVIEWS = 10;
+
 type ReviewListProps = {
   reviews: Review[];
   authorizationStatus: AuthorizationStatus;
@@ -15,7 +17,7 @@ export default function ReviewsList({ reviews, authorizationStatus, offerId }: R
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews_list">
-        {reviews.map((review) => (
+        {reviews.slice(0, MAX_REVIEWS).map((review) => (
           <ReviewItem key={review.id} review={review} />
         ))}
         {authorizationStatus === AuthorizationStatus.Auth && <CommentForm offerId={offerId} />}
