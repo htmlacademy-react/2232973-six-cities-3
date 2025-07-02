836a41d6598e3642378c2204d32e2fc6379cc601
keka <kvtka4enko@yandex.ru>
2025-07-02 18:56:38 +0300

    fix(favoritesPage): уберет отображение h1 тега на пустой странице

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -25,24 +25,25 @@ export default function FavouritesPage(): JSX.Element {
     <main className="page__main page__main--favorites">
       <div className="page__favorites-container container">
         <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
           {offers.length > 0 ? (
-            <ul className="favorites__list">
-              {Object.entries(groupedOffers).map(([cityName, cityOffers]) => (
-                <li className="favorites__locations-items" key={cityName}>
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>{cityName}</span>
-                      </a>
+            <><h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {Object.entries(groupedOffers).map(([cityName, cityOffers]) => (
+                  <li className="favorites__locations-items" key={cityName}>
+                    <div className="favorites__locations locations locations--current">
+                      <div className="locations__item">
+                        <a className="locations__item-link" href="#">
+                          <span>{cityName}</span>
+                        </a>
+                      </div>
                     </div>
-                  </div>
-                  <div className="favorites__places">
-                    <OffersList offers={cityOffers} variant="horizontal" />
-                  </div>
-                </li>
-              ))}
-            </ul>
+                    <div className="favorites__places">
+                      <OffersList offers={cityOffers} variant="horizontal" />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </>
           ) : (
             <div className="favorites__status-wrapper">
               <b className="favorites__status">Nothing yet saved.</b>
